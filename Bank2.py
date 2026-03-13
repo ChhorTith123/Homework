13cba8be78c9c4d2e6c4c90380c5c63782d27660
ChhorTith123 <khaychhortith12@gmail.com>
2026-03-13 08:15:19 +0700

    Update again Bank2.py

--- Bank2.py
+++ Bank2.py
@@ -1,43 +1,44 @@
 class BankAccount:
     def __init__(self, name, balance, secret):
         self.name = name
-        self._balance = balance
-        self._secret = secret
+        self.balance = balance
+        self.secret = secret
 
     def verify(self, secret):
-        return self._secret == secret
+        return self.secret == secret
 
     def withdraw(self, amount):
-        if amount > self._balance:
+        if amount > self.balance:
             print("Balance is not enough")
             return
-        self._balance -= amount
+        self.balance -= amount
         print("Withdraw successfully!")
-        print(f"Your remaining balance is {self._balance}")
+        print(f"Your remaining balance is {self.balance}")
 
     def deposit(self, amount):
-        self._balance += amount
+        self.balance += amount
         print("Deposit Successfully!")
-        print(f"Your remaining balance is {self._balance}")
+        print(f"Your remaining balance is {self.balance}")
 
     def check_balance(self):
-        print(f"Hello {self.name}. Your remaining balance is {self._balance}")
+        print(f"Hello {self.name}. Your remaining balance is {self.balance}")
 
+    ####
     def transfer(self, receiver, amount):
-
-        if amount > self. balance:
-           print("You dont have enough balance to transfer") 
-           return
+        if amount > self.balance:
+            print("You don't have enough balance to transfer")
+            return
         self.withdraw(amount)
         receiver.deposit(amount)
-        print("Transaction successfully.")
+        print("Transfer successful")
+    ####
 
 
 class SavingAccount(BankAccount):
     def calculate_interest(self):
-        self._balance += 10
+        self.balance += 10
         print("Interest Received: 10$")
-        print(f"Your remaining balance is {self._balance}")
+        print(f"Your remaining balance is {self.balance}")
 
 
 class StudentAccount(BankAccount):
@@ -51,32 +52,33 @@ class StudentAccount(BankAccount):
 class BusinessAccount(BankAccount):
     def take_loan(self, amount):
         print("You took loan successfully!")
-        self._balance += amount
-        print(f"Thank you! Your balance now is {self._balance}")
+        self.balance += amount
+        print(f"Thank you! Your balance now is {self.balance}")
 
 
 accounts = {}
 current_user = None
 
 while True:
-    print('''
-=== ATM System ===
-Choose Menu
-1. CREATE ACCOUNT
-2. LOGIN
-3. CHECK BALANCE
-4. WITHDRAW
-5. DEPOSIT
-6. TRANSFER
-7. LOGOUT
-''')
+    print("""
+    --- ATM System ---
+    Choose Menu
+
+    1. CREATE ACCOUNT
+    2. LOGIN
+    3. CHECK BALANCE
+    4. WITHDRAW
+    5. DEPOSIT
+    6. TRANSFER
+    7. LOGOUT
+    """)
 
     choice = input("Select Menu: ")
 
     if choice == "1":
         name = input("Input account name: ")
         if name in accounts:
-            print("Account name already exists.")
+            print("Account name is exist")
             continue
 
         balance = float(input("Input initial balance: "))
@@ -92,17 +94,14 @@ Choose Menu
         secret = input("Input secret PIN: ")
 
         if name in accounts and accounts[name].verify(secret):
-            print("")
-            print(f"Login success! Welcome {name}!")
-            print("")
+            print(f"Login success! Welcome {name}")
             current_user = accounts[name]
-            continue
-
-        print("Invalid account name or secret")
+        else:
+            print("Invalid account name or secret")
 
     elif choice == "3":
         if current_user is None:
-            print("Please login first!!")
+            print("Please login first")
             continue
         current_user.check_balance()
 
@@ -112,18 +111,19 @@ Choose Menu
             continue
         amount = float(input("Input amount: "))
         current_user.withdraw(amount)
+
     elif choice == "5":
         if current_user is None:
             print("Please login first")
             continue
-        amount = float(input("Input amount to deposit: "))
+        amount = float(input("Input amount: "))
         current_user.deposit(amount)
 
     elif choice == "6":
         if current_user is None:
             print("Please login first")
             continue
-        
+
         receiver_name = input("Input receiver: ")
         if receiver_name not in accounts:
             print("Receiver doesn't exist")
@@ -134,8 +134,4 @@ Choose Menu
 
     elif choice == "7":
         current_user = None
-        print("Logout success !!")
-
-    else:
-
-        print("Invalid menu")
+        print("Logout success!")
